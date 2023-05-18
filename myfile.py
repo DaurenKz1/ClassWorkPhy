5835d9a5f6a91ef410b07b0dd35f2165168442ba
DaurenKz1 <daur.ronaldojuve77@gmail.com>
2023-05-18 21:45:03 +0600

    Update myfile.py
    решил 5 задач и начал 6

--- myfile.py
+++ myfile.py
@@ -1,1 +1,28 @@
-print(10)
+#a = int(input("Введите число: "))
+#b = a**2
+#print("Результат: ",b)
+
+#a = int(input("Введите 1 число: "))
+#b = int(input("Введите 2 число: "))
+#print("Результат: ", (a+b)/2)
+
+#dl = int(input("Введите длину: "))
+#s = dl**2
+#print("Площадь квадрата:", s)
+
+#km = int(input("Введите км: "))
+#print("Результат: ", km*0.6211371)
+
+#a = int(input("Введите 1 число: "))
+#b = int(input("Введите 2 число: "))
+#print(a+b,a-b,a*b,a/b)
+
+#a = int(input("Введите число: "))
+#b = int(input("Введите число: "))
+#print("x равно -", b/a)
+
+
+#hour = int(input("Введите часы: "))
+#minute = int(input("Введите минуты: "))
+#day = hour * 60 + minute
+#ost 
